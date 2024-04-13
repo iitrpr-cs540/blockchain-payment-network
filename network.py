8875aa468ac63af33215a225143a74421dbf17cb
debjit <debjitbhowal.db@gmail.com>
2024-04-13 20:10:51 +0530

    transaction utils like next hop and termination handlers

--- network.py
+++ network.py
@@ -24,3 +24,55 @@ class Network(PCNN):
             bid_value.append([self.get_bid_for_node(node, destination), neighbor])
         min_bid = min(bid_value, key=lambda x: x[0])
         return min_bid
+
+class Transaction:
+    stack: list[HTLCBid]
+    current_node: str
+    src: str
+    dest: str
+    amount: int
+    network: Network
+    hop_count: int
+    tx_id: str
+
+    def __init__(self, src, dest, amount, network: Network, id):
+        self.stack = []
+        self.current_node = src
+        self.src = src
+        self.dest = dest
+        self.amount = amount
+        self.network = network
+        self.hop_count = 0
+        self.tx_id: id
+
+    def next_hop(self):
+        if (self.network.notification(self.current_node, self.dest)):
+            self.stack.append(HTLCBid(amount=self.amount, transaction_fee=0, security_deposit=0, hop_count=self.hop_count+1, src=self.current_node, dest=self.dest, tx_id=self.tx_id))
+            self.network.node_probabilities[self.current_node].chosen(self.dest)
+            return True, True
+        else:
+            try:
+                bid = self.network.bidding(self.current_node, self.dest)
+                self.stack.append(HTLCBid(amount=self.amount, transaction_fee=bid[0], security_deposit=self.network.alpha * bid[0], hop_count=self.hop_count+1, src=self.current_node, dest=bid[1], tx_id=self.tx_id))
+                self.network.node_probabilities[self.current_node].chosen(bid[1])
+                self.hop_count += 1
+                self.current_node = bid[1]
+                return True, False
+            except ValueError as e:
+                print(e)
+                return False, None
+        # returns hop_success, has_ended
+        # when hop_success returns as false, run reject()
+        # when hop_success returns true, true, run resolve()
+        
+    def resolve(self):
+        while len(self.stack) > 0:
+            bid = self.stack.pop()
+            bid.resolve(self.network.payment_channels[self.tx_id], node_probabilities=self.network.node_probabilities[self.current_node])
+        print(f"{self.network.env.now}: Transaction completed successfully.")
+
+    def reject(self):
+        while len(self.stack) > 0:
+            bid = self.stack.pop()
+            bid.reject(self.network.payment_channels[self.tx_id])
+        print(f"{self.network.env.now}: Transaction failed to reach {self.dest} from {self.src}.")
