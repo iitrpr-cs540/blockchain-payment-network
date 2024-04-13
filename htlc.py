2c2d1aff9a4e4f083916aa66ee678891d2dab25a
debjit <debjitbhowal.db@gmail.com>
2024-04-13 20:11:15 +0530

    htlc bid class changed

--- htlc.py
+++ htlc.py
@@ -1,19 +1,27 @@
+from pcnn import PCNN, NodeProbabilities
+
+MAX_HOP = 5
 class HTLCBid:
-    def __init__(self, amount, transaction_fee,security_deposit, lock_time, transfer_time, bid_id, state='open'):
+    def __init__(self, amount, transaction_fee, security_deposit, hop_count, src, dest, tx_id):
+        if hop_count > MAX_HOP:
+            raise ValueError("Hop count exceeds maximum allowed")
+
         self.amount = amount
         self.transaction_fee = transaction_fee
         self.security_deposit = security_deposit
-        self.lock_time = lock_time
-        self.transfer_time = transfer_time
-        self.bid_id = bid_id
-        self.state = state
+        self.hop_count = hop_count
+        self.state = "locked"
+        self.src = src
+        self.dest = dest
+        self.tx_id = tx_id
 
-    def lock(self):
-        self.state = 'locked'
+    def resolve(self, payment_channel, node_probabilities: NodeProbabilities):
+        self.state = 'completed'
+        payment_channel[self.src] -= self.amount + self.transaction_fee
+        payment_channel[self.dest] += self.amount + self.transaction_fee
+        node_probabilities[self.src].add_successes(self.dest)
 
-    def unlock(self, success=True):
-        if success:
-            self.state = 'completed'
-        else:
-            # Logic to handle security deposits on failure
-            self.state = 'failed'
+    def reject(self, payment_channel):
+        self.state = 'rejected'
+        payment_channel[self.src] += self.security_deposit
+        payment_channel[self.dest] -= self.security_deposit
